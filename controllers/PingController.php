e93b5cc8056a09e0a21c7c8ea300ae935859cf96
Gayrat Usmonov <rabdavinci@yandex_at_ru>
2020-07-10 10:10:53 +0500

    Added Ping Controller

---
+++ controllers/PingController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class PingController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * pong.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        Yii::$app->response->statusCode = 201;
+
+        $obj = new \stdClass();
+        $obj->request = 'ping';
+        $obj->response =  Yii::$app->response->statusCode;
+        
+        return json_encode($obj);
+    }
+}
